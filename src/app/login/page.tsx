c7368864379c5be41e596a613b138792ca88f2ea
cliffordaustin <106864832+cliffordaustin@users.noreply.github.com>
2024-05-17 05:24:10 +0000

    updates

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,12 +2,13 @@
 
 import Login from "@/components/Login";
 import React from "react";
+import { Suspense } from "react";
 
 function VerifyUserProfile() {
   return (
-    <>
+    <Suspense>
       <Login></Login>
-    </>
+    </Suspense>
   );
 }
 
